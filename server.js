a4560e0d1e5f3b2799a561b9b6dfe2921871fca9
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-12 14:00:03 +0100

    Send information about the client at the given api route (Completion of core project focus)

--- server.js
+++ server.js
@@ -33,3 +33,13 @@ var listener = app.listen(8000, function () {
 
 
 //APP
+app.get("/api/whoami", (req, res) => {
+	const ip = req.ip;
+	const language = req.headers["accept-language"];
+	const software = req.headers["user-agent"];
+	res.json({
+		ipaddress: ip,
+		language: language,
+		software: software
+	});
+});
